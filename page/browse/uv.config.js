c5715a41f7b19abb3134bfc1f2cc1c79457a5e6f
defrostee <defrostdevs@atomicmail.io>
2026-03-23 16:57:20 -0400

    Update uv.config.js

--- page/browse/uv.config.js
+++ page/browse/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/service/',
-  bare: '/bare/',
+  bare: 'https://eugenex-server.onrender.com/bare/',  // ← absolute URL, correct path
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: '/uv/uv.handler.js',
